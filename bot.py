7f127f26e20ec1ae6a87659ef9859839acfa8a07
flatypus <68029599+flatypus@users.noreply.github.com>
2021-10-01 14:22:02 -0700

    Update bot.py
    added embed images with title

--- bot.py
+++ bot.py
@@ -5,17 +5,21 @@ import random
 
 client = discord.Client()
 
+def gettext(info):
+    return(((info).split('>')[1]).split('<')[0])
+
 def getxkcdquote():
     URL = f"https://xkcd.com/{random.randint(1,2000)}/"
     page = requests.get(URL)
     content = BeautifulSoup(page.content, "lxml")
+    title = str(content.find(id='ctitle'))
     a = content.find_all("a")
     picturelink = ''
     for _ in a:
         item = str(_)
         if "https://imgs.xkcd.com/" in item:
             picturelink = item
-    return(((picturelink).split('>')[1]).split('<')[0])
+    return([gettext(picturelink),gettext(title)])
 
 @client.event
 async def on_ready():
@@ -26,7 +30,10 @@ async def on_message(message):
     if message.author == client.user:
         return
     if message.content == '!xkcd':
-        xkcd = getxkcdquote()
-        await message.channel.send(xkcd)        
+        image = getxkcdquote()[0]
+        title = getxkcdquote()[1]
+        embed=discord.Embed(title=title)
+        embed.set_image(url=image)
+        await message.channel.send(embed=embed)      
 
-client.run(YOUR_TOKEN_HERE)
+client.run(YOUR TOKEN GOES HERE)
